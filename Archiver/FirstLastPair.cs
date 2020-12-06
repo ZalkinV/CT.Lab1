efc842ce690983fb634e7629e993dac2d78a4aa4
Zalkin <Zalkin.viktor@mail.ru>
2020-12-07 01:49:15 +0300

    Coder. Create FirstLastPair

---
+++ Archiver/FirstLastPair.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archiver
+{
+    public struct FirstLastPair
+    {
+        public byte First;
+        public byte Last;
+        public int OriginIndex;
+
+        public FirstLastPair(byte first, byte last, int originIndex)
+        {
+            this.First = first;
+            this.Last = last;
+            this.OriginIndex = originIndex;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is FirstLastPair pair &&
+                   this.First == pair.First &&
+                   this.Last == pair.Last &&
+                   this.OriginIndex == pair.OriginIndex;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.First, this.Last, this.OriginIndex);
+        }
+    }
+}
