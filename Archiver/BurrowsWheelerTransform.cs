b349889de66a7ea10c7e9d4f828c19caa24b15f0
Zalkin <Zalkin.viktor@mail.ru>
2020-12-07 02:13:37 +0300

    Encoder. Create BurrowsWheelerTransform

---
+++ Archiver/BurrowsWheelerTransform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archiver
+{
+    public static class BurrowsWheelerTransform
+    {
+        public static FirstLastPair[] GetAllShifts(byte[] bytes)
+        {
+            int bytesCount = bytes.Length;
+            FirstLastPair[] pairs = new FirstLastPair[bytes.Length];
+            for (int i = 0; i < bytesCount; i++)
+            {
+                byte curByte = bytes[i];
+                int lastByteIndex = (i + bytesCount - 1) % bytesCount;
+                byte lastByte = bytes[lastByteIndex];
+
+                pairs[i] = new FirstLastPair(curByte, lastByte, i);
+            }
+
+            return pairs;
+        }
+    }
+}
--- Archiver/Encoder.cs
+++ Archiver/Encoder.cs
@@ -15,18 +15,8 @@ namespace Archiver
 
         public FirstLastPair[] BWT(byte[] bytes)
         {
-            int bytesCount = bytes.Length;
-            FirstLastPair[] pairs = new FirstLastPair[bytes.Length];
-            for (int i = 0; i < bytesCount; i++)
-            {
-                byte curByte = bytes[i];
-                int lastByteIndex = (i + bytesCount - 1) % bytesCount;
-                byte lastByte = bytes[lastByteIndex];
-
-                pairs[i] = new FirstLastPair(curByte, lastByte, i);
-            }
-
-            return pairs;
+            var unsortedPairs = BurrowsWheelerTransform.GetAllShifts(bytes);
+            return unsortedPairs;
         }
     }
 }
