98249386790eff7c87ebd957f93fd88e767fe4de
Zalkin <Zalkin.viktor@mail.ru>
2020-12-08 17:26:49 +0300

    Decoder. Create decoder with BWT

---
+++ Archiver/Decoder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archiver
+{
+    public class Decoder
+    {
+        public IList<byte> Bytes { get; set; }
+
+        public Decoder(IList<byte> bytes)
+        {
+            this.Bytes = bytes;
+        }
+
+        public IList<byte> Decode()
+        {
+            return this.Bytes;
+        }
+
+        public IList<byte> BWT(int initialStringIndex)
+        {
+            var bwt = new BurrowsWheelerTransform(this.Bytes);
+            var reversedBytes = bwt.InverseTransform(initialStringIndex);
+
+            return reversedBytes;
+        }
+    }
+}
