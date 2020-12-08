7b6c9f1c83b6ef6231cf7726f9227bd7a3955e74
Zalkin <Zalkin.viktor@mail.ru>
2020-12-08 15:18:00 +0300

    BWT. Add using standard sorting and use it creating bwt

--- Archiver/BurrowsWheelerTransform.cs
+++ Archiver/BurrowsWheelerTransform.cs
@@ -16,71 +16,26 @@ namespace Archiver
 
         public BurrowsWheelerResult DirectTransform()
         {
-            var firstLastPairs = GetAllShifts(this.Bytes);
-            var arr = firstLastPairs.Select(p => p.OriginIndex).ToArray();
-            Array.Sort(arr, CompareShifts);
-
-            int initialStringIndex = 0;
-            byte[] lastSymbols = new byte[firstLastPairs.Count];
-            for (int i = 0; i < firstLastPairs.Count; i++)
-            {
-                if (firstLastPairs[i].OriginIndex == 0)
-                    initialStringIndex = i;
-                lastSymbols[i] = firstLastPairs[i].Last;
-            }
-
-            var bwtResult = new BurrowsWheelerResult(lastSymbols, initialStringIndex);
-
-            return bwtResult;
-        }
-
-        public static IList<FirstLastPair> GetAllShifts(IList<byte> bytes)
-        {
-            int bytesCount = bytes.Count;
-            FirstLastPair[] pairs = new FirstLastPair[bytesCount];
-            for (int i = 0; i < bytesCount; i++)
-            {
-                byte curByte = bytes[i];
-                int lastByteIndex = (i + bytesCount - 1) % bytesCount;
-                byte lastByte = bytes[lastByteIndex];
-
-                pairs[i] = new FirstLastPair(curByte, lastByte, i);
-            }
-
-            return pairs;
-        }
-
-        public void Sort(IList<FirstLastPair> pairs)
-        {
             int bytesCount = this.Bytes.Count;
 
-            for (int i = 1; i < pairs.Count; i++)
+            int[] firstByteIndexes = new int[bytesCount];
+            for (int i = 0; i < firstByteIndexes.Length; i++)
+                firstByteIndexes[i] = i;
+
+            Array.Sort(firstByteIndexes, CompareShifts);
+            int initialStringIndex = Array.IndexOf(firstByteIndexes, 0);
+
+            byte[] lastBytes = new byte[bytesCount];
+            for (int i = 0; i < firstByteIndexes.Length; i++)
             {
-                int j = i - 1;
-
-                FirstLastPair pairToPut = pairs[i];
-                while (j >= 0 && pairs[j].First >= pairToPut.First)
-                {
-                    int leftIndex = pairs[j].OriginIndex;
-                    int rightIndex = pairToPut.OriginIndex;
-                    while (this.Bytes[leftIndex] == this.Bytes[rightIndex])
-                    {
-                        leftIndex = (leftIndex + 1) % bytesCount;
-                        rightIndex = (rightIndex + 1) % bytesCount;
-
-                        // If all substring chars are equal
-                        if (leftIndex == pairToPut.OriginIndex || rightIndex == pairs[j].OriginIndex)
-                            break;
-                    }
-
-                    if (this.Bytes[leftIndex] < this.Bytes[rightIndex]) break;
-
-                    pairs[j + 1] = pairs[j];
-                    j--;
-                }
-
-                pairs[j + 1] = pairToPut;
+                int firstByteIndex = firstByteIndexes[i];
+                int lastByteIndex = (firstByteIndex + bytesCount - 1) % bytesCount;
+                lastBytes[i] = this.Bytes[lastByteIndex];
             }
+
+            var bwtResult = new BurrowsWheelerResult(lastBytes, initialStringIndex);
+
+            return bwtResult;
         }
 
         private int CompareShifts(int indexOfLeft, int indexOfRight)
