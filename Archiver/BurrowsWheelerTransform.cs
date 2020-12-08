205a2f8255d08a18061e47698afab2decc9dd790
Zalkin <Zalkin.viktor@mail.ru>
2020-12-08 03:17:14 +0300

    BWT. Add Sort() with insertion sort

--- Archiver/BurrowsWheelerTransform.cs
+++ Archiver/BurrowsWheelerTransform.cs
@@ -36,5 +36,38 @@ namespace Archiver
 
             return pairs;
         }
+
+        public void Sort(IList<FirstLastPair> pairs)
+        {
+            int bytesCount = this.Bytes.Count;
+
+            for (int i = 1; i < pairs.Count; i++)
+            {
+                int j = i - 1;
+
+                FirstLastPair pairToPut = pairs[i];
+                while (j >= 0 && pairs[j].First >= pairToPut.First)
+                {
+                    int leftIndex = pairs[j].OriginIndex;
+                    int rightIndex = pairToPut.OriginIndex;
+                    while (this.Bytes[leftIndex] == this.Bytes[rightIndex])
+                    {
+                        leftIndex = (leftIndex + 1) % bytesCount;
+                        rightIndex = (rightIndex + 1) % bytesCount;
+
+                        // If all substring chars are equal
+                        if (leftIndex == pairToPut.OriginIndex || rightIndex == pairs[j].OriginIndex)
+                            break;
+                    }
+
+                    if (this.Bytes[leftIndex] < this.Bytes[rightIndex]) break;
+
+                    pairs[j + 1] = pairs[j];
+                    j--;
+                }
+
+                pairs[j + 1] = pairToPut;
+            }
+        }
     }
 }
