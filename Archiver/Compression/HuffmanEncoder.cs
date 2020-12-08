a653135ed9709a5384f5ccc2e487473502d1e1f4
Zalkin <Zalkin.viktor@mail.ru>
2020-12-09 00:43:23 +0300

    Huffman. Create HuffmanEncoder without full impl and HuffmanNode

---
+++ Archiver/Compression/HuffmanEncoder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Archiver.Compression
+{
+    public class HuffmanEncoder
+    {
+        public IList<byte> Bytes { get; set; }
+
+        public IList<int> Counts { get; set; }
+        public Dictionary<byte, int> Codes { get; set; }
+
+        public HuffmanEncoder(IList<byte> bytes)
+        {
+            this.Bytes = bytes;
+            this.Counts = new int[256];
+            this.Codes = new Dictionary<byte, int>();
+        }
+
+        public void Count()
+        {
+            int bytesCount = this.Bytes.Count;
+            for (int i = 0; i < bytesCount; i++)
+            {
+                byte curByte = this.Bytes[i];
+                this.Counts[curByte]++;
+            }
+        }
+
+        public void BuildCodes()
+        {
+            int countsLength = this.Counts.Count;
+            List<HuffmanNode> nodes = new List<HuffmanNode>(countsLength);
+            for (int i = 0; i < countsLength; i++)
+            {
+                byte curByte = (byte)i;
+                int curCount = this.Counts[i];
+                if (curCount != 0)
+                {
+                    HuffmanNode node = new HuffmanNode(curCount, curByte);
+                    nodes.Add(node);
+                }
+            }
+            nodes.Sort(CompareNodes);
+        }
+
+        public static int CompareNodes(HuffmanNode left, HuffmanNode right)
+        {
+            return -left.Count.CompareTo(right.Count);
+        }
+
+        public IList<byte> Encode()
+        {
+            return this.Bytes;
+        }
+    }
+}
---
+++ Archiver/Compression/HuffmanNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archiver.Compression
+{
+    public class HuffmanNode
+    {
+        public int Count { get; set; }
+        public byte[] Symbols { get; set; }
+
+        public HuffmanNode(int count, byte[] symbols)
+        {
+            this.Count = count;
+            this.Symbols = symbols;
+        }
+
+        public HuffmanNode(int count, byte symbol)
+            : this(
+                  count: count,
+                  symbols: new byte[] { symbol })
+        {
+
+        }
+
+        public static HuffmanNode Merge(HuffmanNode left, HuffmanNode right)
+        {
+            int sumCount = left.Count + right.Count;
+            byte[] bytes = new byte[left.Symbols.Length + right.Symbols.Length];
+            Array.Copy(left.Symbols, bytes, left.Symbols.Length);
+            Array.Copy(right.Symbols, 0, bytes, left.Symbols.Length, right.Symbols.Length);
+
+            HuffmanNode node = new HuffmanNode(sumCount, bytes);
+            return node;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Count} {string.Join(' ', this.Symbols)}";
+        }
+    }
+}
