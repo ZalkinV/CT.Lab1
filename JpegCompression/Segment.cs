c7eeeb0fc43de9f0926648314fe74db2e02a1dae
Zalkin <Zalkin.viktor@mail.ru>
2020-12-20 21:34:25 +0300

    JC. Create Segment class

---
+++ JpegCompression/Segment.cs
@@ -0,0 +1,19 @@
+﻿namespace JpegCompression
+{
+    public class Segment
+    {
+        public double Left { get; set; }
+        public double Right { get; set; }
+
+        public Segment(double left, double right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Left} {this.Right}";
+        }
+    }
+}
