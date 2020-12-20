1ffae5223738c2cc45f780a252aea0ec0c2855d1
Zalkin <Zalkin.viktor@mail.ru>
2020-12-20 22:07:31 +0300

    JC/AC. Add link to neerc.ifmo

--- JpegCompression/ArithmeticCoder.cs
+++ JpegCompression/ArithmeticCoder.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace JpegCompression
 {
+    //https://neerc.ifmo.ru/wiki/index.php?title=%D0%90%D1%80%D0%B8%D1%84%D0%BC%D0%B5%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5_%D0%BA%D0%BE%D0%B4%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5
     public class ArithmeticCoder
     {
         Dictionary<byte, int> SymbolsWeights { get; set; }
