868caf0a494fdc6a6f57fb460945dd36e7758fd4
Zalkin <Zalkin.viktor@mail.ru>
2020-12-21 12:32:15 +0300

    JC/AC. Add link to useful source with CABAC

--- JpegCompression/ArithmeticCoder.cs
+++ JpegCompression/ArithmeticCoder.cs
@@ -6,6 +6,7 @@ using System.Text;
 namespace JpegCompression
 {
     //https://neerc.ifmo.ru/wiki/index.php?title=%D0%90%D1%80%D0%B8%D1%84%D0%BC%D0%B5%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5_%D0%BA%D0%BE%D0%B4%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5
+    //http://bertolami.com/index.php?engine=blog&content=posts&detail=arithmetic-coding
     public class ArithmeticCoder
     {
         Dictionary<byte, int> Weights { get; set; }
